e090ca0ca016126636e1861b6a7a6d2ac2ccc5b9
Jainne Silva <jainnesilva150@gmail.com>
2022-09-21 09:00:18 -0300

    Aula 3.1

---
+++ Aula03_1/src/aula03/Caneta1.java
@@ -0,0 +1,23 @@
+package aula03;
+public class Caneta1 {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+    void status() {
+        System.out.println("Uma caneta " + this.cor);
+    }
+            
+    void rabiscar() {
+        
+    }
+    
+    void tampar() {
+        
+    }
+    
+    void destampar() {
+        
+    }
+}
