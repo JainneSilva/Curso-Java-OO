f8b9546dfa029bae1718f099c7c8ed6b510973c0
Jainne Silva <jainnesilva150@gmail.com>
2022-09-26 11:44:16 -0300

    Aula 06

---
+++ Aula06/src/aula06/Aula06.java
@@ -0,0 +1,11 @@
+package aula06;
+public class Aula06 {
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.menosVolume();
+        c.abrirMenu();
+        c.fecharMenu();
+    }
+    
+}
