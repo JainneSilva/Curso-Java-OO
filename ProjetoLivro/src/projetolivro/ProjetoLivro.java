e0134c1ef0821334fa7a74868e2a4c0bd14772a4
Jainne Silva <jainnesilva150@gmail.com>
2022-09-29 18:11:33 -0300

    Aula 09a - Exercício Prático

---
+++ ProjetoLivro/src/projetolivro/ProjetoLivro.java
@@ -0,0 +1,22 @@
+package projetolivro;
+public class ProjetoLivro {
+    public static void main(String[] args) {
+       Pessoa[] p = new Pessoa[2];
+       Livro[] l = new Livro[3];
+       
+       p[0] = new Pessoa("Pedro", 22, "M");
+       p[1] = new Pessoa("Maria", 25, "F");
+       
+       l[0] = new Livro("Aprendendo Java", "José da Silva", 300, p[0]);
+       l[1] = new Livro("P00 para Iniciantes", "Pedro Paulo", 500, p[1]);
+       l[2] = new Livro("Java Avançado", "Maria Candido", 800, p[0]);
+       
+       l[0].abrir();
+       l[0].folhear(100);
+       l[0].avancarPag();
+        System.out.println(l[0].detalhes());
+        System.out.println("==================================================");
+        System.out.println(l[1].detalhes());
+    }
+    
+}
